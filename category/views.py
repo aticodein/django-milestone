d533d6aad660e84b796d2b724421be4593ec6801
Attila Szaloki <szalokiattila@gmail.com>
2020-03-25 19:59:45 +0000

    working category filter at categories.html

--- category/views.py
+++ category/views.py
@@ -3,25 +3,26 @@ from .models import Category
 
 # Create your views here.
 
-
-"""
-def BootstrapFilterView(request):
-    cta = Category.objects.all()
-    print(cta)
-    title_contains_query = request.GET.get('title_contains')
-    id_exact_query = request.GET.get('id_exact')
-    context = {
-        "queryset": qs,
-    }
-    return render(request, "categories.html", context)
-"""
+def not_none(param):
+    return param != '' and param != None
 
 def all_categories(request):
     categories = Category.objects.all()
     title_contains_query = request.GET.get('title_contains')
-    print(title_contains_query)
-    id_exact_query = request.GET.get('id_exact')
-    print(id_exact_query)
+    description = request.GET.get('description')
+    category = request.GET.get('category')
+
+
+    if not_none(title_contains_query):
+       categories = categories.filter(title__icontains=title_contains_query)
+
+    elif not_none(description):
+       categories = categories.filter(description__icontains=description)
+
+    elif not_none(category) and category != 'All Categories':
+       categories = categories.filter(title__icontains=category)
+
+
     context = {
         "categories": categories
     }
