b0952ee889883ecdbe021fc65698e54e44ce417d
Attila Szaloki <szalokiattila@gmail.com>
2020-04-03 22:30:46 +0000

    if else in settings for databases

--- online_auction/settings.py
+++ online_auction/settings.py
@@ -101,14 +101,23 @@ WSGI_APPLICATION = 'online_auction.wsgi.application'
 #    }
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
-DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       }
-}
+# DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+# }
 
-
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
