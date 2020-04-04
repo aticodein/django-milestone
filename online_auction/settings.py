21942b40daf5ff333a038be84db057c6fd142fcc
Attila Szaloki <szalokiattila@gmail.com>
2020-04-04 15:54:04 +0000

    new settings

--- online_auction/settings.py
+++ online_auction/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('GITPOD_HOSTNAME','localhost'), 'milestone-4-django-project.herokuapp']
+ALLOWED_HOSTS = [os.environ.get('GITPOD_HOSTNAME','localhost'), 'milestone-4-django-project.herokuapp.com']
 
 
 # Application definition
